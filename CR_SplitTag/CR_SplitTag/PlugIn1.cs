ea98543b819e1174c230f4fdb8ebf81ded5c4f05
Rory Becker <RoryBecker@gmail.com>
2014-09-29 13:04:14 +0100

    Initial Load.

---
+++ CR_SplitTag/CR_SplitTag/PlugIn1.cs
@@ -0,0 +1,62 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.CodeRush.Core;
+using DevExpress.CodeRush.PlugInCore;
+using DevExpress.CodeRush.StructuralParser;
+using SP = DevExpress.CodeRush.StructuralParser;
+
+namespace CR_SplitTag
+{
+    public partial class PlugIn1 : StandardPlugIn
+    {
+        // DXCore-generated code...
+        #region InitializePlugIn
+        public override void InitializePlugIn()
+        {
+            base.InitializePlugIn();
+            registerSplitTag();
+        }
+        #endregion
+        #region FinalizePlugIn
+        public override void FinalizePlugIn()
+        {
+            //
+            // TODO: Add your finalization code here.
+            //
+
+            base.FinalizePlugIn();
+        }
+        #endregion
+        public void registerSplitTag()
+        {
+            DevExpress.CodeRush.Core.CodeProvider SplitTag = new DevExpress.CodeRush.Core.CodeProvider(components);
+            ((System.ComponentModel.ISupportInitialize)(SplitTag)).BeginInit();
+            SplitTag.ProviderName = "SplitTag"; // Should be Unique
+            SplitTag.DisplayName = "Split Tag";
+            SplitTag.CheckAvailability += SplitTag_CheckAvailability;
+            SplitTag.Apply += SplitTag_Apply;
+            ((System.ComponentModel.ISupportInitialize)(SplitTag)).EndInit();
+        }
+        private void SplitTag_CheckAvailability(Object sender, CheckContentAvailabilityEventArgs ea)
+        {
+            SP.HtmlElement tag = (SP.HtmlElement) ea.Element.Parent;
+            if (tag == null)
+                return;
+            ea.Available = true;
+        }
+
+        private void SplitTag_Apply(Object sender, ApplyContentEventArgs ea)
+        {
+            // This method is executed when the system executes your Code 
+            SP.HtmlElement tag = (SP.HtmlElement)ea.Element.Parent;
+            string tagSplitter = String.Format("</{0}>{1}<{0}>", tag.Name, System.Environment.NewLine);
+            ea.TextDocument.InsertText(ea.Selection.StartSourcePoint, tagSplitter);
+        }
+
+        
+    }
+}
---
+++ CR_SplitTag/CR_SplitTag/PlugIn1VsixExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.Composition;
+using DevExpress.CodeRush.Common;
+
+namespace CR_SplitTag
+{
+    [Export(typeof(IVsixPluginExtension))]
+    public class CR_SplitTagExtension : IVsixPluginExtension { }
+}
